affe68a6fb581f4523ca58ede7a0c49ebb22828f
Anish <anishkalpakunj@gmail.com>
2019-11-14 12:09:28 +0545

    Laravel defaul Login Success For admin login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Auth;
 class AdminController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
+        if($request->isMethod('post')){
+            $data=$request->input();
+            if(Auth::attempt(['email'=>$data['email'],'password'=>$data['password'],'admin'=>'1'])){
+                echo "Success";
+                die;
+            }
+            else{
+                echo "Fait";
+                die;
+            }
+        }
         return view('admin.admin_login');
     }
 }
