8af8964d7ce72fc5c5c0284dcf1ef9964e1e9bdb
Anish <anishkalpakunj@gmail.com>
2019-11-14 09:51:52 +0545

    Create LAravel Default Login/Register

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ Route::get('/', function () {
 });
 
 Route::get('/admin','AdminController@login');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
