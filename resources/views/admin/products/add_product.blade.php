83f1d6780f03999a51511b075b4f6f81a7abfabd
Anish <anishkalpakunj@gmail.com>
2019-11-21 20:06:01 +0545

    Product Added Successfully

--- resources/views/admin/products/add_product.blade.php
+++ resources/views/admin/products/add_product.blade.php
@@ -5,6 +5,19 @@
   <div id="content-header">
     <div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a href="#">Products</a> <a href="#" class="current">Add Product</a> </div>
     <h1>Products</h1>
+    @if(Session::has('flash_message_error'))  
+        <div class="alert alert-danger alert-block">
+	<button type="button" class="close" data-dismiss="alert">×</button>	
+        <strong>{!! session('flash_message_error') !!}</strong>
+</div>   
+        @endif   
+
+        @if(Session::has('flash_message_success'))  
+        <div class="alert alert-success alert-block">
+	<button type="button" class="close" data-dismiss="alert">×</button>	
+        <strong>{!! session('flash_message_success') !!}</strong>
+</div>   
+        @endif  
   </div>
   <div class="container-fluid"><hr>
     <div class="row-fluid">
@@ -14,11 +27,11 @@
             <h5>Add Product</h5>
           </div>
           <div class="widget-content nopadding">
-            <form class="form-horizontal" method="post" action="{{ url('/admin/add-product')}}" name="add_category" id="add_category" novalidate="novalidate">{{ csrf_field()}}
+            <form class="form-horizontal" method="post" action="{{ url('/admin/add-product')}}" name="add_product" id="add_product" novalidate="novalidate">{{ csrf_field()}}
               <div class="control-group">
                 <label class="control-label">Under Category</label>
                 <div class="controls">
-                  <select name = "category_id" style="width: 220px">
+                  <select name = "category_id" id = "category_id" style="width: 220px">
                     <?php echo $categories_dropdown; ?>
                   </select>
                 </div>
