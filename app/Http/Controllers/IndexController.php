99e5c4f474eea7e8856b6b5632604e7260ff1aad
Anish <anishkalpakunj@gmail.com>
2019-11-23 23:20:55 +0545

    Successfully show Product in home page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class IndexController extends Controller
 {
     public function index(){
-        return view('index');
+        // //In Ascending Order By Default
+        // $productsAll = Product::get();
+        // // In Descending Order
+        // $productsAll = Product::orderby('id','DESC')->get();
+
+        //In Random
+        $productsAll = Product::inRandomOrder('id','DESC')->get();
+        return view('index')->with(compact('productsAll'));
     }
 }
